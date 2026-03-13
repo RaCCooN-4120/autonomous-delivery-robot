b9192fb1f38fc93c57573c60c59510a4e839cb2e
RaCCooN-4120 <rawi.thongsong@gmail.com>
2026-03-14 04:52:49 +0700

    clean ros2 build files

--- delivery_ws/build/delivery_robot/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/raccoon/autonomous-delivery-robot/delivery_ws/install/delivery_robot'
